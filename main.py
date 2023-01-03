e8253115e148989a8f4dfed81f3300b74a4c1e23
Normand <normandlaredo@gmail.com>
2023-01-03 16:15:37 +0000

    adding folder by defult for certifications

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import csv
 from docxtpl import DocxTemplate
 
+# Configuration
 template = DocxTemplate('certificate-template.docx')
 filename = 'data.csv'
 output = '/certifications'
 
+# get all rows
 getList = []
 
+# Open and reading csv
 with open(filename, 'r') as data:
     for line in csv.reader(data, delimiter=','):
         getList.append(line)
 
 
+# Function to create files .docx
 def create_certification():
     for names in getList[2:]:
+        # Column's Name
         date = names[0]
         name = names[1]
         head_event = names[2]
@@ -27,4 +32,6 @@ def create_certification():
         template.render(context)
         template.save(f".{output}/{name}.docx")
 
+
+# Run function
 create_certification()
